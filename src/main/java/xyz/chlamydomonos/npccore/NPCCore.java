6a9ea3617569e575278045b1af5a3ee598e57a8e
Chlamydomonos <2457242458@qq.com>
2023-06-23 22:09:39 +0800

    Initial Commit

---
+++ src/main/java/xyz/chlamydomonos/npccore/NPCCore.java
@@ -0,0 +1,11 @@
+package xyz.chlamydomonos.npccore;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(NPCCore.MODID)
+public class NPCCore
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "npccore";
+}
